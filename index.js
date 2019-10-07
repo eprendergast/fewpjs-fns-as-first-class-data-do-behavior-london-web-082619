e71b4b47b837f8e5d35b265021df95a124a5548c
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-10-07 13:48:28 +0100

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,26 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(time) {
+
+  const hour = parseInt(time.split(":"));
+
+  if (hour < 12) {
+    return "Good Morning";
+  } 
+  if (hour > 17) {
+    return "Good Evening";
+  } 
+  else {
+    return "Good Afternoon"
+  }
+
+}
+
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(text) {
+  document.querySelector("#greeting").innerText = text;
+}
